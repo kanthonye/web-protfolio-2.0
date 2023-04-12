6fd1a18f39b69dedb4bad97e1b4e1a298ab2956b
kanthonye <kanthonye@live.com>
2023-04-12 00:57:46 -0400

    navbar update

--- js/myscript.js
+++ js/myscript.js
@@ -1,18 +1,18 @@
 
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function()
-// {
-//     var currentScrollPos = window.pageYOffset;
-//     if (prevScrollpos > currentScrollPos)
-//     {
-//         document.getElementById("navbar").style.top = "0";
-//     }
-//     else
-//     {
-//         document.getElementById("navbar").style.top = "-70px";
-//     }
-//     prevScrollpos = currentScrollPos;
-// }
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function()
+{
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos - 1)
+    {
+        document.getElementById("navbar").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("navbar").style.display = "none";
+    }
+    prevScrollpos = currentScrollPos;
+}
 
 function MouseOut( elem, name )
 {
